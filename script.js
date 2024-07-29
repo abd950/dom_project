3fd1d32e6c1b56a3f64abe3d94261df03aa33e04
abd950 <jahabdulrahman1000@gmail.com>
2024-07-29 15:42:53 -0400

    added changes

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Add your code to this file
+let mainHeader = document.querySelector("#mainTitle");
+
+let mainDiv = document.querySelector("div");
+mainDiv.style.border = "8px solid red";
+
+let mainList = document.querySelector("div");
+mainPokemondescription.style.border = "blue";
+
+
